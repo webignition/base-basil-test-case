3ebd39323c9676882cffd5a1815dc5294e0f9042
Jon Cram <webignition@gmail.com>
2020-08-25 13:03:10 +0100

    Catch browser start failures, retry before ultimately failing (#91)

---
+++ src/ClientManager.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BaseBasilTestCase;
+
+use Symfony\Component\Panther\Client;
+use webignition\BasilModels\Test\ConfigurationInterface;
+
+class ClientManager
+{
+    public const STATE_UNKNOWN = 0;
+    public const STATE_SUCCEEDED = 1;
+    public const STATE_FAILED = 2;
+    public const START_ATTEMPT_LIMIT = 3;
+
+    private const BROWSER_CHROME = 0;
+    private const BROWSER_FIREFOX = 1;
+    private const LABEL_CHROME = 'chrome';
+    private const LABEL_FIREFOX = 'firefox';
+
+    private const CLIENT_ID_MAP = [
+        self::LABEL_FIREFOX => self::BROWSER_FIREFOX,
+        self::LABEL_CHROME => self::BROWSER_CHROME,
+    ];
+
+    private ?\Throwable $lastException = null;
+    private ConfigurationInterface $configuration;
+    private Client $client;
+    private int $failedStartAttemptCount = 0;
+
+    public function __construct(ConfigurationInterface $configuration)
+    {
+        $this->configuration = $configuration;
+
+        $browserLabel = $this->configuration->getBrowser();
+        $clientId = self::CLIENT_ID_MAP[$browserLabel] ?? self::BROWSER_CHROME;
+
+        if (self::BROWSER_FIREFOX === $clientId) {
+            $this->client = Client::createFirefoxClient();
+        } else {
+            $this->client = Client::createChromeClient();
+        }
+    }
+
+    public function getConfiguration(): ConfigurationInterface
+    {
+        return $this->configuration;
+    }
+
+    public function getClient(): Client
+    {
+        return $this->client;
+    }
+
+    public function start(): int
+    {
+        $browserStartState = self::STATE_UNKNOWN;
+
+        while (
+            self::STATE_UNKNOWN === $browserStartState &&
+            false === $this->hasStartAttemptLimitBeenReached()
+        ) {
+            try {
+                $this->client->start();
+                $browserStartState = self::STATE_SUCCEEDED;
+            } catch (\Throwable $exception) {
+                $this->lastException = $exception;
+
+                if (false === $this->isBrowserStartException($exception)) {
+                    $browserStartState = self::STATE_FAILED;
+                } else {
+                    $this->failedStartAttemptCount++;
+                }
+            }
+        }
+
+        if ($this->hasStartAttemptLimitBeenReached()) {
+            $browserStartState = self::STATE_FAILED;
+        }
+
+        return $browserStartState;
+    }
+
+    public function getLastException(): ?\Throwable
+    {
+        return $this->lastException;
+    }
+
+    private function isBrowserStartException(\Throwable $exception): bool
+    {
+        $message = $exception->getMessage();
+
+        if (substr_count($message, 'failed to start') > 0) {
+            return true;
+        }
+
+        if (substr_count($message, 'Could not start') > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function hasStartAttemptLimitBeenReached(): bool
+    {
+        return self::START_ATTEMPT_LIMIT === $this->failedStartAttemptCount;
+    }
+}
---
+++ tests/Unit/ClientManagerTest.php
@@ -0,0 +1,160 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BaseBasilTestCase\Tests\Unit;
+
+use Facebook\WebDriver\WebDriver;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Panther\Client;
+use Symfony\Component\Panther\ProcessManager\BrowserManagerInterface;
+use Symfony\Component\Panther\ProcessManager\ChromeManager;
+use Symfony\Component\Panther\ProcessManager\FirefoxManager;
+use webignition\BaseBasilTestCase\ClientManager;
+use webignition\BasilModels\Test\Configuration;
+use webignition\BasilModels\Test\ConfigurationInterface;
+use webignition\ObjectReflector\ObjectReflector;
+
+class ClientManagerTest extends TestCase
+{
+    public function testCreateFireFoxClient()
+    {
+        $configuration = new Configuration('firefox', 'http://example.com');
+
+        $clientManager = new ClientManager($configuration);
+        self::assertSame($configuration, $clientManager->getConfiguration());
+        self::assertNull($clientManager->getLastException());
+
+        $client = $clientManager->getClient();
+        self::assertInstanceOf(Client::class, $client);
+        self::assertInstanceOf(FirefoxManager::class, $client->getBrowserManager());
+    }
+
+    /**
+     * @dataProvider createDataProvider
+     */
+    public function testCreateChromeClient(ConfigurationInterface $configuration)
+    {
+        $clientManager = new ClientManager($configuration);
+        self::assertSame($configuration, $clientManager->getConfiguration());
+        self::assertNull($clientManager->getLastException());
+
+        $client = $clientManager->getClient();
+        self::assertInstanceOf(Client::class, $client);
+        self::assertInstanceOf(ChromeManager::class, $client->getBrowserManager());
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            'chrome' => [
+                'configuration' => new Configuration('chrome', 'http://example.com'),
+            ],
+            'unknown' => [
+                'configuration' => new Configuration('unknown', 'http://example.com'),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider startDataProvider
+     */
+    public function testStart(
+        ?int $startSuccessIteration,
+        \Throwable $startFailureException,
+        int $expectedBrowserStartState,
+        ?\Throwable $expectedLastException,
+        int $expectedFailedStartAttemptCount
+    ) {
+        $configuration = new Configuration('chrome', 'http://example.com');
+        $clientManager = new ClientManager($configuration);
+
+        $browserManager = \Mockery::mock(BrowserManagerInterface::class);
+        $browserManager
+            ->shouldReceive('quit');
+
+        $webDriver = \Mockery::mock(WebDriver::class);
+        $webDriver
+            ->shouldReceive('quit');
+
+        $browserManager
+            ->shouldReceive('start')
+            ->andReturnUsing(
+                function () use ($startFailureException, $webDriver, $startSuccessIteration) {
+                    static $counter = 0;
+                    $counter++;
+
+                    if ($counter === $startSuccessIteration) {
+                        return $webDriver;
+                    }
+
+                    throw $startFailureException;
+                }
+            );
+
+        $client = new Client($browserManager);
+
+        ObjectReflector::setProperty($clientManager, ClientManager::class, 'client', $client);
+
+        $browserStartState = $clientManager->start();
+
+        $this->assertClientManagerPostStartState(
+            $clientManager,
+            $expectedBrowserStartState,
+            $browserStartState,
+            $expectedLastException,
+            $expectedFailedStartAttemptCount
+        );
+    }
+
+    public function startDataProvider(): array
+    {
+        $startFailureException = new \RuntimeException('Could not start chrome (or it crashed) after 30 seconds.');
+
+        return [
+            'succeeds on first try' => [
+                'startSuccessIteration' => 1,
+                'startFailureException' => $startFailureException,
+                'expectedBrowserStartState' => ClientManager::STATE_SUCCEEDED,
+                'expectedLastException' => null,
+                'expectedFailedStartAttemptCount' => 0,
+            ],
+            'succeeds on second try' => [
+                'startSuccessIteration' => 2,
+                'startFailureException' => $startFailureException,
+                'expectedBrowserStartState' => ClientManager::STATE_SUCCEEDED,
+                'expectedLastException' => $startFailureException,
+                'expectedFailedStartAttemptCount' => 1,
+            ],
+            'succeeds on third try' => [
+                'startSuccessIteration' => 3,
+                'startFailureException' => $startFailureException,
+                'expectedBrowserStartState' => ClientManager::STATE_SUCCEEDED,
+                'expectedLastException' => $startFailureException,
+                'expectedFailedStartAttemptCount' => 2,
+            ],
+            'fails after third try' => [
+                'startSuccessIteration' => null,
+                'startFailureException' => $startFailureException,
+                'expectedBrowserStartState' => ClientManager::STATE_FAILED,
+                'expectedLastException' => $startFailureException,
+                'expectedFailedStartAttemptCount' => 3,
+            ],
+        ];
+    }
+
+    private function assertClientManagerPostStartState(
+        ClientManager $clientManager,
+        int $expectedBrowserStartState,
+        int $browserStartState,
+        ?\Throwable $expectedLastException,
+        int $expectedFailedStartAttemptCount
+    ): void {
+        self::assertSame($expectedBrowserStartState, $browserStartState);
+        self::assertSame($expectedLastException, $clientManager->getLastException());
+        self::assertSame(
+            $expectedFailedStartAttemptCount,
+            ObjectReflector::getProperty($clientManager, 'failedStartAttemptCount')
+        );
+    }
+}
