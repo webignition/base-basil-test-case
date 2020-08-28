5762b649b4780165043affa7f4715929d9cdbabc
Jon Cram <webignition@gmail.com>
2020-08-28 15:19:10 +0100

    Add BasilTestCaseInterface::staticSetLastException() (#97)

--- src/BasilTestCaseInterface.php
+++ src/BasilTestCaseInterface.php
@@ -40,7 +40,8 @@ interface BasilTestCaseInterface extends Test
     public function getExaminedElementIdentifier(): ?ElementIdentifierInterface;
     public function getExpectedElementIdentifier(): ?ElementIdentifierInterface;
 
-    public static function setLastException(\Throwable $exception): void;
+    public static function staticSetLastException(\Throwable $exception): void;
+    public function setLastException(\Throwable $exception): void;
     public static function staticGetLastException(): ?\Throwable;
     public function getLastException(): ?\Throwable;
 
