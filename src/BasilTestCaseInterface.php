46edd8184002e1e78f18020cded400aa684e73d1
Jon Cram <webignition@gmail.com>
2022-01-12 16:38:57 +0000

    Use php-cs-fixer, update phpstan (#122)

--- src/BasilTestCaseInterface.php
+++ src/BasilTestCaseInterface.php
@@ -12,6 +12,7 @@ use webignition\DomElementIdentifier\ElementIdentifierInterface;
 interface BasilTestCaseInterface extends Test
 {
     public function setBasilStepName(string $stepName): void;
+
     public function getBasilStepName(): string;
 
     public function getStatus(): int;
@@ -22,28 +23,38 @@ interface BasilTestCaseInterface extends Test
     public function getHandledStatements(): array;
 
     public function setExaminedValue(?string $examinedValue): void;
+
     public function setExpectedValue(?string $expectedValue): void;
 
     public function getExaminedValue(): ?string;
+
     public function getExpectedValue(): ?string;
 
     public function setBooleanExaminedValue(bool $examinedValue): void;
+
     public function setBooleanExpectedValue(bool $expectedValue): void;
 
     public function getBooleanExaminedValue(): ?bool;
+
     public function getBooleanExpectedValue(): ?bool;
 
     public function getExaminedElementIdentifier(): ?ElementIdentifierInterface;
+
     public function getExpectedElementIdentifier(): ?ElementIdentifierInterface;
 
     public static function staticSetLastException(\Throwable $exception): void;
+
     public function setLastException(\Throwable $exception): void;
+
     public static function staticGetLastException(): ?\Throwable;
+
     public function getLastException(): ?\Throwable;
 
     public function setCurrentDataSet(?DataSetInterface $dataSet): void;
+
     public function getCurrentDataSet(): ?DataSetInterface;
 
     public static function setClientManager(ClientManager $clientManager): void;
+
     public static function hasException(): bool;
 }
