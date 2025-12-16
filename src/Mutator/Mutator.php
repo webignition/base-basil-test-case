6b175093ebe39ad6527cb5dfcf43ef21b312d191
Jon Cram <webignition@gmail.com>
2025-12-16 18:25:15 +0000

    Import webignition/webdriver-element-mutator (#198)

---
+++ src/Mutator/Mutator.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace webignition\BaseBasilTestCase\Mutator;
+
+use Facebook\WebDriver\WebDriverElement;
+use webignition\WebDriverElementCollection\RadioButtonCollection;
+use webignition\WebDriverElementCollection\SelectOptionCollection;
+use webignition\WebDriverElementCollection\WebDriverElementCollectionInterface;
+
+class Mutator
+{
+    private const string SELECT_TAG_NAME = 'select';
+    private const string VALUE_ATTRIBUTE_NAME = 'value';
+
+    public function setValue(WebDriverElementCollectionInterface $collection, string $value): void
+    {
+        if ($collection instanceof RadioButtonCollection || $collection instanceof SelectOptionCollection) {
+            $this->setSelectedCollectionValue($collection, $value);
+
+            return;
+        }
+
+        if (1 === count($collection)) {
+            $element = $collection->get(0);
+
+            if ($element instanceof WebDriverElement) {
+                $this->setElementValue($element, $value);
+
+                return;
+            }
+        }
+    }
+
+    private function setElementValue(WebDriverElement $element, string $value): void
+    {
+        $tagName = $element->getTagName();
+
+        if (self::SELECT_TAG_NAME === $tagName) {
+            $selectOptionCollection = SelectOptionCollection::fromSelectElement($element);
+
+            if ($selectOptionCollection instanceof SelectOptionCollection) {
+                $this->setSelectedCollectionValue($selectOptionCollection, $value);
+            }
+
+            return;
+        }
+
+        $element->clear();
+        $element->sendKeys($value);
+    }
+
+    private function setSelectedCollectionValue(WebDriverElementCollectionInterface $collection, string $value): void
+    {
+        foreach ($collection as $item) {
+            $valueAttribute = trim((string) $item->getAttribute(self::VALUE_ATTRIBUTE_NAME));
+
+            if ($value === $valueAttribute) {
+                $item->click();
+            }
+        }
+    }
+}
