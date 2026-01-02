c0c3fa1499688ed152450e75d9ef883f7e479908
Jon Cram <webignition@gmail.com>
2026-01-02 14:39:51 +0000

    Statements attribut expects an array of jsonified action, assertion (#211)

--- src/Attribute/Statements.php
+++ src/Attribute/Statements.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace webignition\BaseBasilTestCase\Attribute;
 
+use webignition\BasilModels\Model\Action\ActionInterface;
+use webignition\BasilModels\Model\Assertion\AssertionInterface;
+
+/**
+ * @phpstan-import-type SerializedAction from ActionInterface
+ * @phpstan-import-type SerializedAssertion from AssertionInterface
+ */
 #[\Attribute(\Attribute::TARGET_METHOD)]
 readonly class Statements
 {
     /**
-     * @param array{'type':'action'|'assertion', 'statement':non-empty-string}[] $statements
+     * @param array<SerializedAction|SerializedAssertion> $statements
      */
     public function __construct(public array $statements) {}
 }
