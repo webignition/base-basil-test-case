605a829b41bf76ad38be4beffe6cc7e62f73e52c
Jon Cram <webignition@gmail.com>
2026-01-05 14:01:50 +0000

    Remove dependency on basil-models

--- src/Attribute/Statements.php
+++ src/Attribute/Statements.php
@@ -4,19 +4,10 @@ declare(strict_types=1);
 
 namespace webignition\BaseBasilTestCase\Attribute;
 
-use webignition\BasilModels\Model\Action\ActionInterface;
-use webignition\BasilModels\Model\Assertion\AssertionInterface;
-
-/**
- * @phpstan-import-type SerializedAction from ActionInterface
- * @phpstan-import-type SerializedAssertion from AssertionInterface
- */
 #[\Attribute(\Attribute::TARGET_METHOD)]
 readonly class Statements
 {
     /**
-     * Each string must be a json-serialized SerializedAction or SerializedAssertion.
-     *
      * @param string[] $statements
      */
     public function __construct(public array $statements) {}
