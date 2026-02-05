5fa8421b5e382dcd986e77d3ef1fccd12ee0c0b8
Jon Cram <webignition@gmail.com>
2026-02-05 15:34:00 +0000

    Hold data names and values in attribute

--- src/Attribute/Data.php
+++ src/Attribute/Data.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace webignition\BaseBasilTestCase\Attribute;
 
 #[\Attribute(\Attribute::TARGET_METHOD)]
-readonly class DataNames
+readonly class Data
 {
     /**
-     * @param string[] $names
+     * @param array<int, array<int|string, scalar>> $data
      */
-    public function __construct(public array $names) {}
+    public function __construct(public array $data) {}
 }
